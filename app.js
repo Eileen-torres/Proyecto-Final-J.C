df1e39b0d4c6fbe4d03e33b0ec3cad365f7d045b
Eileen-torres <etorrescharris@gmail.com>
2024-11-23 00:42:09 -0500

    css
    le agregue css al html y ademas  la manera responsiva

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-if(document.getElementById('register-form')) {
-    document.getElementById('register-form').addEventListener("submit", (event)=>) {
-        event.preventDefault()
+document.getElementById("formRegistro").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        const username = document.getElementById('username'). value
-        const password = document.getElementById('password'). value
+    const nombre = document.getElementById("nombre").Value;
+    const apellido = document.getElementById("apellido").Value;
+    const correo = document.getElementById("correo").value;
+    const contraseña = document.getElementById("contraseña").value;
+    const genero = document.getElementById("genero").value;
 
-        if(username && password ) {
-            localStorage.setItem("user", JSON.stringify({username, password}))
-            alert('usuario registrado correctamente')
-            window.location.href = 'login.html'
-        }else {
-            alert('por favor, complete los campos')
-        }
-        }
 
-    }
+    const usuario = {
+        nombre: nombre,
+        apellido: apellido,
+        correo: correo,
+        contraseña: contraseña,
+        genero: genero
+    };
+
+    localStorage.setItem("usuarioRegistrado", JSON.stringify(usuario));
+    window.location.href = "login.html"
+
 }
 
+//login// 
+
+document.getElementById("formRegistro").addEventListener("submit", function (event) {
+    event.preventDefault ();
+
+    const correologin = document.getElementById("correologin").value;
+    const contraseñalogin = document.getElementById("contraseñalogin").value;
+
+    const usuarioRegistrado = JSON.parse(localStorage.getItem(usuarioRegistrado));
+
+    if (usuarioRegistrado && usuarioRegistrado.correo === correologin && usuarioRegistrado.
+        contraseña === contraseñalogin) {
+            localStorage.setItem("nombreUsuario, usuarioRegistrado.nombre");
+            window.location.href = "saludo.html";
+        } else {
+            alert("correo o contraseña incorrectos");
+        }
+        ))
+}
